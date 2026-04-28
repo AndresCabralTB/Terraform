06443b740f18d9d6d9251db9030a70b043c3bc03
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-04-27 19:53:13 -0600

    Update: Setting up optional VPN

--- Container-Startup.sh
+++ Container-Startup.sh
@@ -1,7 +1,7 @@
-docker run --name mydocker-container-v13-4040 --restart=on-failure --detach \
+docker run --name mydocker-container-v14-4040 --restart=on-failure --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --env JENKINS_OPTS="--httpPort=4040" \
   --publish 4040:4040 --publish 60000:60000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  docker-images-v13:latest
+  docker-images-v14:latest
--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Setting up ngrok on http 4040" >> ngrok_output.log
+
 # Start ngrok in the background
-ngrok http 4040 --authtoken $NGROK_AUTHTOKEN &
+ngrok http 4040
+
+echo "Ngrok started" >> ngrok_output.log
 
 /usr/local/bin/jenkins.sh
