51821e120e3255afe5357fb6ea8d1734952f7844
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-04-29 23:49:53 -0600

    Restart

--- Infrastructures/Infrastructure-VPN/VPC-Conf/Internet-Gateway.tf
+++
@@ -1,9 +0,0 @@
-#Create the internet gateway and assign it to the VPC
-resource "aws_internet_gateway" "InternetGateway" {
-  vpc_id = aws_vpc.VPC_Terraform.id
-
-  tags = {
-    Name = "Internet-Gateway-Terraform-${var.project_version}"
-  }
-  
-}
