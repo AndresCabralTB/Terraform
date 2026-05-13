8ccbe16dbf6ffdfd7af11b3c37220a14cc2471bc
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-05-13 08:04:29 -0600

    Plan VPN Enabled with S3 Bucket

---
+++ Infrastructure/Client-VPN-Conf/S3-Bucket.tf
@@ -0,0 +1,21 @@
+variable "project_version" {
+  type = string
+}
+
+resource "aws_s3_bucket" "vpn_configs_bucket" {
+  bucket = "cloud-cabral-ovpn-files"
+  force_destroy = true
+  tags = {
+    Name        = "VPN Configs"
+    Version     = var.project_version
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "vpn_configs" {
+  bucket                  = aws_s3_bucket.vpn_configs_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on              = [aws_s3_bucket.vpn_configs_bucket]
+}
