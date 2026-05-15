18e5d5801a6f1aa832f8334d058d0802d7838623
AndresCabralTB <andres.cabralrs@udlap.mx>
2026-05-15 16:48:10 -0600

    Commiting V1

--- Docker/Docker-Conf/ECR-Conf.tf
+++ Docker/Docker-Conf/ECR-Conf.tf
@@ -3,7 +3,7 @@ variable "project_environment" {
 }
 
 resource "aws_ecr_repository" "ecr_repo"{
-    name    = "ecr-repo-${var.project_environment}"
+    name    = "docker-images-repo-${var.project_environment}"
     image_tag_mutability = "IMMUTABLE"
     image_scanning_configuration {
         scan_on_push = true
