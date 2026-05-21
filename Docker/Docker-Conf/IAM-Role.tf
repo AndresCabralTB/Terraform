01023aa2492165961c05450a963440ea4b7a13c4
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-05-20 22:37:15 -0600

    Deploy PROD - DOCKER | DEBUG 15.0

--- Docker/Docker-Conf/IAM-Role.tf
+++ Docker/Docker-Conf/IAM-Role.tf
@@ -40,7 +40,8 @@ resource "aws_iam_role_policy" "ECS-RolePolicy" {
                     "logs:CreateLogGroup",
                     "logs:CreateLogStream",
                     "logs:PutLogEvents",
-                    "logs:DescribeLogStreams"
+                    "logs:DescribeLogStreams",
+                    "logs:TagResource"
                 ],
                 Resource = [
                     "arn:aws:logs:*:*:*"
