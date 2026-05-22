5215a50dd98c4abfec529436850edd962b0c11ac
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-05-21 23:27:21 -0600

    Commit

--- Bootstrap/S3-Bucket-Conf/S3-Bucket.tf
+++ Bootstrap/S3-Bucket-Conf/S3-Bucket.tf
@@ -43,3 +43,12 @@ resource "aws_s3_bucket" "docker-volumes"{
         prevent_destroy = true
     }
 }
+
+resource "aws_s3_bucket_public_access_block" "private_bucket_access" {
+  bucket = aws_s3_bucket.docker-volumes.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
