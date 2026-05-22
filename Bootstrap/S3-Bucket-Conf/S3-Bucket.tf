c7ecee757494ea8f10a0c16f1a3ad62cfb1e65c7
AndrescabralTB <andres.cabralrs@udlap.mx>
2026-05-21 23:41:43 -0600

    Commit

--- Bootstrap/S3-Bucket-Conf/S3-Bucket.tf
+++ Bootstrap/S3-Bucket-Conf/S3-Bucket.tf
@@ -35,11 +35,11 @@ resource "aws_s3_bucket" "jenkins-project-infrastructure"{
 }
 
 resource "aws_s3_bucket" "docker-volumes" {
-  bucket = "docker-volumes-bucket-718254829448"
-  tags = {
-    Name = "docker-volumes-bucket-718254829448"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
+    bucket = "docker-volumes-bucket-cabral"
+    tags = {
+        Name = "docker-volumes-bucket-cabral"
+    }
+    lifecycle {
+        prevent_destroy = true
+    }
 }
