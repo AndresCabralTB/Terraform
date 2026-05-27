246ffd7ab5089292fd823afbb4d3171f7db9b5f9
AndresCabralTB <andres.cabralrs@udlap.mx>
2026-05-27 13:57:03 -0600

    Addind user data to BastionHost | Debug 9.0

--- Infrastructure/EC2-Conf/Startup_script.sh
+++ Infrastructure/EC2-Conf/Startup_script.sh
@@ -3,10 +3,9 @@ DNS_NAME="${DNS_NAME}"
 MOUNT_DIR="${MOUNT_DIR}"
 INTERNAL_NAME="${INTERNAL_NAME}"
 
-mkdir -p "$MOUNT_DIR"
-chown 1000:1000 "$MOUNT_DIR"
+mkdir -p "${MOUNT_DIR}"
+chown 1000:1000 "${MOUNT_DIR}"
 
-hostnamectl set-hostname "$INTERNAL_NAME"
-
-sudo mount -t efs -o tls "$DNS_NAME":/ "$MOUNT_DIR"
+hostnamectl set-hostname "${INTERNAL_NAME}"
+mount -t efs -o tls "${DNS_NAME}":/ "${MOUNT_DIR}"
 
