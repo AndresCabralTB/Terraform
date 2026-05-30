b787e49af79c5d2dd12e8701bfee10ba865110f7
AndresCabralTB <andres.cabralrs@udlap.mx>
2026-05-29 18:35:37 -0600

    New Docker Image

--- Bootstrap/IAM-Instance-Profile/IAM-Instance-Profile.tf
+++ Bootstrap/IAM-Instance-Profile/IAM-Instance-Profile.tf
@@ -33,7 +33,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2FullAccess" {
         "arn:aws:iam::aws:policy/EC2InstanceConnect",
         "arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess",
         "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-        "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+        "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 
     ])
     policy_arn = each.value
